ccfff35339ae7d72905fe2fb61867b576bd335f6
James Ruston <jruston90@gmail.com>
2014-03-12 15:45:56 +0000

    refactored transformer spec

--- test/featureTransformerSpec.js
+++ test/featureTransformerSpec.js
@@ -2,36 +2,32 @@ var featureTransformer = require('../lib/featureTransformer'),
     fs = require('fs'),
     chai = require('chai'),
     assert = require('chai').assert;
-chai.use(require('chai-fuzzy'));;
+chai.use(require('chai-fuzzy'));
 
 describe("Tranformation", function() {
 
     it("transforms a single IT", function(done) {
         var inputFile = "test/fixtures/spec/exampleSpec.feature";
-        var tempOut =  "test/fixtures/test-output/tmp.json"
         var expectedOutput = {
             "should do something" : "test/fixtures/spec/exampleSpec.feature:1"
         }
-
-        featureTransformer(inputFile, tempOut, function() {
-            fs.readFile(tempOut, 'utf8', function (err, data) {
-                if (err) throw err;
-                assert.notEqual(data, "");
-                assert.like(JSON.parse(data), expectedOutput);
-                done();
-            });
-        });
+        verifyFileOutput(inputFile, expectedOutput, done);
         
     });
 
     it("transforms a describe", function(done) {
         var inputFile = "test/fixtures/spec/describe.feature";
-        var tempOut =  "test/fixtures/test-output/tmp.json";
         var expectedOutput = {
             "a description" : {
                 "should do something" : "test/fixtures/spec/describe.feature:2"
             }
         }
+        verifyFileOutput(inputFile, expectedOutput, done);
+        
+    });
+
+    function verifyFileOutput(inputFile, expectedOutput, done) {
+        var tempOut =  "test/fixtures/test-output/tmp.json";
 
         featureTransformer(inputFile, tempOut, function() {
             fs.readFile(tempOut, 'utf8', function (err, data) {
@@ -41,6 +37,5 @@ describe("Tranformation", function() {
                 done();
             });
         });
-        
-    });
+    }
 });
