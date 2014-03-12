0aa4ae74599770f589d775fa5dad017e58107b84
Richard McIntyre <richard.mackstar@gmail.com>
2014-03-12 08:48:35 +0000

    Adding comparison file requirement.

--- test/configBuilderSpec.js
+++ test/configBuilderSpec.js
@@ -5,18 +5,24 @@ var buildConfig = require("../lib/configBuilder"),
 describe ("Config Builder", function () {
 
 	beforeEach(function(){
-		args = ["", "", "", ""];
+		args = ["", "", "", "", ""];
 	});
 
-	it("should be able to have where the input file is set", function(){
+	it("should be able to take a set input file", function(){
 		var inputFile = "tests/fixtures/spec/exampleSpec.feature";
 		args[2] = inputFile;
 		assert.equal(buildConfig(args).inputFile, inputFile);
 	});
 
+	it("should be able to take a set file to compare to", function(){
+		var comparisonFile = "tests/fixtures/spec/exampleSpec.feature";
+		args[3] = comparisonFile;
+		assert.equal(buildConfig(args).comparisonFile, comparisonFile);
+	});
+
 	it("should be able to have an output file when set", function(){
 		var outputFile = "tests/fixtures/test-output/output.tmp.json";
-		args[3] = outputFile;
+		args[4] = outputFile;
 		assert.equal(buildConfig(args).outputFile, outputFile);
 	});
 
@@ -25,8 +31,13 @@ describe ("Config Builder", function () {
 		assert.throws(function() {buildConfig(args)}, Error, "An input file must be specified");
 	});
 
-	it("should raise an exeption if the output file isnt passed", function() {
+	it("should raise an exeption if the comparison file isnt passed", function() {
 		args[3] = undefined;
+		assert.throws(function() {buildConfig(args)}, Error, "A comparison file must be specified");
+	});
+
+	it("should raise an exeption if the output file isnt passed", function() {
+		args[4] = undefined;
 		assert.throws(function() {buildConfig(args)}, Error, "An output file must be specified");
 	});
 });
