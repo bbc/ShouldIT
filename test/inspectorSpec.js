4a5bc157cc668e3a86660c19282f6781d645f752
Richard McIntyre <richard.mackstar@gmail.com>
2014-03-12 16:41:58 +0000

    Simple tests passing for inspector

--- test/inspectorSpec.js
+++ test/inspectorSpec.js
@@ -1,13 +1,52 @@
-var inspector = require('../lib/inspector'),
+(function(require){
+
+    var inspector = require('../lib/inspector'),
     assert = require('assert');
 
-describe("Inspector", function() {
-    it("should take 2 files content in a list", function(done) {
-        var inputFile = "test/fixtures/test-output/jasmine-output1.json";
-        var comparisonFile = "test/fixtures/test-output/jasmine-output1.json";
-        inspector(inputFile, comparisonFile, function(files){
-            assert.equal(files.length, 2);
-            done();
+
+    describe("Inspector", function() {
+        it("should tell when a match is found", function() {
+            var expected = {
+                "passed": [{
+                    "describe something should do something": "test/fixtures/spec/exampleSpec.feature:1"
+                }],
+                "pending": []
+            }
+            objectsEqual(inspector(filesContent()), expected);
+        });
+
+        it("should tell when a match is not found", function() {
+
+            var expected = {
+                "passed": [],
+                "pending": [{
+                    "describe something should do something": "test/fixtures/spec/exampleSpec.feature:1"
+                }]
+            },
+            files = filesContent();
+            files[1]["describe something"] = {"should do something else" : "PASSED"};
+            objectsEqual(inspector(files), expected);
         });
     });
-});
+
+    /* Local Methods */
+
+    function objectsEqual(obj1, obj2) {
+        assert.equal(
+            JSON.stringify(obj1), JSON.stringify(obj2)
+        );
+    }
+
+    function filesContent() {
+        return [{
+            "describe something" : {
+                "should do something" : "test/fixtures/spec/exampleSpec.feature:1"
+            }
+        },
+        {
+            "describe something" : {
+                "should do something" : "PASSED"
+            }
+        }];
+    }
+})(require);
