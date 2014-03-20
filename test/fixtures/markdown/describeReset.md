61773ba25c56828c89e9ac1c924e2573c3cbd6f2
James Ruston <jruston90@gmail.com>
2014-03-20 17:10:36 +0000

    Updated markdown parser. Fixed tests.

---
+++ test/fixtures/markdown/describeReset.md
@@ -0,0 +1,5 @@
+# a describe
+## another describe
+* a spec
+# reset describe
+* a second spec
--- test/markdownParserSpec.js
+++ test/markdownParserSpec.js
@@ -1,23 +1,68 @@
-// var markdownParser = require('../lib/markdownParser'),
-//     assert = require('assert');
+var markdownParser = require('../lib/markdownParser'),
+    assert = require('assert');
 
-// describe("Markdown parser", function() {
+describe("Markdown parser", function() {
 
-//     it("callsback with the first spec found", function(done) {
-//         var inputFile = "test/fixtures/markdown/exampleSpec.md";
-//         markdownParser(inputFile, function(spec) {
-//             assert.equal(spec.description, "should do something");
-//             done();
-//         });
-//     });
+    it("callsback with the first spec found", function(done) {
+        var inputFile = "test/fixtures/markdown/exampleSpec.md";
+        markdownParser(inputFile, function(spec) {
+            assert.equal(spec.description, "should do something");
+            done();
+        });
+    });
 
-//     it("adds describe that the spec is nested in", function(done) {
-//         var inputFile = "test/fixtures/markdown/describe.md";
-//         markdownParser(inputFile, function(spec) {
-//             assert.equal(spec.description, "should do something");
-//             assert.equal(spec.suite.length, 1);
-//             assert.equal(spec.suite[0], "a description");
-//             done();
-//         });
-//     });
-// });
+    it("adds describe that the spec is nested in", function(done) {
+        var inputFile = "test/fixtures/markdown/describe.md";
+        markdownParser(inputFile, function(spec) {
+            assert.equal(spec.description, "should do something");
+            assert.equal(spec.suite.length, 1);
+            assert.equal(spec.suite[0], "a description");
+            done();
+        });
+    });
+
+    it("replaces the suite if a header is found at the same level", function(done) {
+        var inputFile = "test/fixtures/markdown/full.md";
+        var count = 0;
+        markdownParser(inputFile, function(spec) {
+            count++;
+            if(count == 2) {
+                assert.equal(spec.description, "a second spec");
+                assert.equal(spec.suite.length, 3);
+                assert.equal(spec.suite[1], "a third description");
+                done();
+            }
+        });
+    });
+
+    // # a describe
+    // ## another description
+    // * a spec         --------> this has two suites, 'a desribe' and 'another describe'
+    // # reset describe
+    // * a second spec   ------> this should only have the suite of 'reset describe'
+    it("removes any suites that have been closed by a smaller level of header", function(done) {
+        var inputFile = "test/fixtures/markdown/describeReset.md";
+        var count = 0;
+        markdownParser(inputFile, function(spec) {
+            count++;
+            if(count == 2) {
+                assert.equal(spec.description, "a second spec");
+                assert.equal(spec.suite.length, 1);
+                assert.equal(spec.suite[0], "reset describe");
+                done();
+            }
+        });
+    });
+
+    // + a spec ----> should be treated as a spec
+    it("handles specs defined with a +", function(done) {
+        var inputFile = "test/fixtures/markdown/skipStep.md";
+        markdownParser(inputFile, function(spec) {
+            assert.equal(spec.description, "a second spec");
+            assert.equal(spec.suite.length, 1);
+            assert.equal(spec.suite[0], "a description");
+            done();
+        });
+    });
+
+});
