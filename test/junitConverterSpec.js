c8cb89f7f135def2005845a4c9389ede47a2ad19
Richard McIntyre <richard.mackstar@gmail.com>
2014-07-01 12:58:45 +0100

    Adding appropriate company name.

--- test/junitConverterSpec.js
+++ test/junitConverterSpec.js
@@ -36,10 +36,23 @@ describe ("JUnit Converter", function () {
         });
     });
 
-    // it("should be able parse a JUnit file that has failures", function(){
-    //     var file = "test/fixtures/junit/javaservice.xml";
-    //     converter = new Converter(file);
-    //     assert.equal(converter.exec(), {});
-    // });
+    it("should be able parse a nexted JUnit file that has failures", function(){
+        var file = "test/fixtures/junit/phpunit.xml";
+        converter = new Converter(file);
+        converter.exec(function (results) {
+            assert.equal(JSON.stringify(results), JSON.stringify({ 'Coverage Api':
+                {
+                    'get returns with 200 status on successful request': 'PASSED',
+                    'get returns with 500 status if repository failure exception': 'PASSED',
+                    'live request in moments repository contains the correct request uri': 'PASSED',
+                    'get returns with 503 status if resource not available exception': 'PASSED',
+                    'get returns with 400 status if id parameter is undefined': 'PASSED',
+                    'get returns transformed response from moment repository': 'PASSED',
+                    'get from content api returns 404': 'PASSED',
+                    'get returns with 400 status if id parameter is empty': 'PASSED'
+                }
+            }));
+        });
+    });
 
 });
