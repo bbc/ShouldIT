6206d5d4586acbaee54005919396bf2ff2abb897
Richard McIntyre <richard.mackstar@gmail.com>
2014-07-07 16:01:06 +0100

    Added improved config builder

--- test/configBuilderSpec.js
+++ test/configBuilderSpec.js
@@ -5,34 +5,34 @@ var buildConfig = require("../lib/configBuilder"),
 describe ("Config Builder", function () {
 
     beforeEach(function(){
-        args = ["", "", "", ""];
+        args = ['node', '/mocha', '--specs=exampleSpec.feature', '--results=results.json,results.xml'];
     });
 
     it("should be able to take a set input file", function(){
         var glob = "tests/fixtures/spec/exampleSpec.feature";
-        args[2] = glob;
-        assert.equal(buildConfig(args).specGlob, glob);
+        args[2] = '--specs=' + glob;
+        assert.equal(buildConfig(args).specs, glob);
     });
 
     it("should be able to take a set file to compare to", function(){
         var comparisonFile = "tests/fixtures/spec/exampleSpec.feature";
-        args[3] = comparisonFile;
-        assert.equal(buildConfig(args).resultsGlob, comparisonFile);
+        args[3] = '--results=' + comparisonFile;
+        assert.equal(buildConfig(args).results, comparisonFile);
     });
 
     it("should raise an exeption if the input file isnt passed", function() {
         args[2] = undefined;
-        assert.throws(function() {buildConfig(args)}, Error, "A glob must be specified");
+        assert.equal(buildConfig(args).message, "A specs glob such as path/*.feature must be specified.");
     });
 
     it("should raise an exeption if the comparison file isnt passed", function() {
         args[3] = undefined;
-        assert.throws(function() {buildConfig(args)}, Error, "A comparison file must be specified");
+        assert.equal(buildConfig(args).message, "A results file must be specified.");
     });
 
-    // it("should be able to add a comma deliminated marker", function() {
-    //     args[4] = undefined;
-    //     assert.throws(function() {buildConfig(args)}, Error, "A comparison file must be specified");
-    // });
+    it("should be able to add a comma deliminated tag", function() {
+        args[4] = "--tags=richard,john";
+        assert.equal(buildConfig(args).tags, ['richard', 'john']);
+    });
 
 });
