c3de33c23be6a059fc207bea24eb4bfc7404a0a9
Richard McIntyre <richard.mackstar@gmail.com>
2015-01-03 22:44:55 +0000

    View builder adapted to new style of output.

--- test/viewBuilderSpec.js
+++ test/viewBuilderSpec.js
@@ -2,48 +2,53 @@ var viewBuilder = require('../lib/viewBuilder'),
     assert = require('assert');
 
 describe("View Builder", function() {
-    it("should take nested examples and pull out the node name", function() {
-        var input = {
-            'A Description': 'some assertion',
-            'A Description I am nested': 'some assertion'
-        }
 
+    var index;
+
+    beforeEach(function () {
+        input = {
+            'A Description': {
+                '_parents': ['A Description'],
+                'specs': {
+                    'some assertion': 'somewhere'
+                }
+            },
+            'A Description I am nested': {
+                '_parents': ['A Description', 'I am nested'],
+                'specs': {
+                    'some assertion': 'somewhere'
+                }
+            }
+        };
+    });
+
+    it("should take nested examples and pull out the node name", function() {
         var view = viewBuilder.build(input);
+
         assert.equal(Object.keys(view.nodes).length, 2);
         assert.equal(view.nodes[0].name, 'A Description');
         assert.equal(view.nodes[1].name, 'I am nested');
+
     });
 
     it("should provide links between nodes", function() {
-        var input = {
-            'A Description': 'some assertion',
-            'A Description I am nested': 'some assertion'
-        }
-
         var view = viewBuilder.build(input);
         assert.deepEqual(view.links[0], {source: 0, target: 1});
     });
 
     it("should supply shoulds", function() {
-        var input = {
-            'A Description': 'some assertion'
-        }
-
         var view = viewBuilder.build(input);
-        assert.deepEqual(view.nodes[0].shoulds, 'some assertion');
+        assert.deepEqual(view.nodes[0].shoulds,  {"some assertion":"somewhere"});
     });
 
-    it("correctly link fuzzy items", function() {
-        var input = {
-            'A Description': 'some assertion',
-            'A Description With Something': 'some assertion',
-            'A Description With Something Else': 'some assertion',
-            'A Description Not Deep As Elsy': 'some assertion'
-        }
+    it("correctly link deeply nested items", function() {
+        delete input['A Description'];
+        input['A Description I am nested']._parents.push('And More Deeply Nested');
 
         var view = viewBuilder.build(input);
-        assert.equal(view.nodes[2].name, 'With Something');
-        assert.deepEqual(view.links[2], { source: 2, target: 3 });
+        assert.equal(Object.keys(view.nodes).length, 3);
+        assert.deepEqual(view.links[0], {source: 0, target: 1});
+        assert.deepEqual(view.links[1], {source: 1, target: 2});
     });
 
 
